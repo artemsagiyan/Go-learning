72e36f749ccf04b025eb97fd7e05a57e2cd1155d
a.sagiyan <artemsagian414@gmail.com>
2024-07-14 17:41:39 +0300

    urlgetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,31 @@
 
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	var command_args []string = os.Args[1:]
+
+	for _, url := range command_args {
+		resp, err := http.Get(url)
+		if err != nil {
+			// defer resp.Body.Close()
+			os.Exit(1)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		check(err)
+
+		_, _ = os.Stdout.Write(body)
+	}
 }
